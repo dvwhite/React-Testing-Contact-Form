d01c77a9da8f128490e729e083bb882d18ec528f
David White <dwhite0627@gmail.com>
2020-02-23 21:06:07 -0500

    Add tests to verify that clicking submit will submit the correct data

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,8 +1,6 @@
 import React, { createRef } from 'react';
-import { fireEvent, render, cleanup, act, wait, getAllByText, getAllByTestId } from "@testing-library/react";
+import { fireEvent, render, cleanup, act } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
-import Enzyme, { mount, find, simulate } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import ContactForm from './ContactForm';
 
 // Tests
@@ -20,12 +18,14 @@ describe('ContactForm renders successfully', () => {
   // before and after each test using beforeEach() and afterEach()
   let getByTestId;
   let getByText;
+  let queryByTestId;
 
   // Setup
   beforeEach(() => {
     const form = render(<ContactForm />);
     getByTestId = form.getByTestId;
     getByText = form.getByText;
+    queryByTestId = form.queryByTestId;
   })
 
   // Teardown
@@ -67,6 +67,11 @@ describe('ContactForm renders successfully', () => {
     const submit = getByText(/submit/i);
     expect(submit).toBeInTheDocument();
   });
+
+  // Prior to submit, the pre tag shouldn't display
+  it (`doesn't render the pre of data before submit is clicked`, () => {
+    expect(queryByTestId('pre-data')).toBeNull();
+  });
 })
 
 describe('submitting succesfully saves data to state', () => {
@@ -82,95 +87,77 @@ describe('submitting succesfully saves data to state', () => {
       const submit = getByText(/submit/i);
       submit.onclick = spy;
       fireEvent.click(submit); // left click
-      expect(spy).toHaveBeenCalledTimes(1);  
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).not.toHaveBeenCalledTimes(0);
     });  
   })
 
   cleanup();
 })
 
-  // Test that data validation occurs in required fields
-describe('Data valudation occurs in the required fields', () => {
-  // Required input fields display validation if length of content < 1
-  let form;
-
-  // Enzyme is handy to fire a simulated submit event to trigger Yup validation
-  Enzyme.configure({ adapter: new Adapter() })
-
-  // Setup
-  beforeEach(() => {
-    form = mount(<ContactForm />);
-  })
-
+describe('test the data is submitted correctly from the form', () => {
+  
   // Teardown
   afterEach(() => {
     cleanup();
   })
 
-  // First name
-  it('displays an error message if first name is blank', () => {
-    
-    act(async () => {
-      form.find('form').simulate('submit');
-      await wait(() => {
-        expect(getAllByTestId(/yup-error-fname/i)).not.toBeNull();
-      })
-    })
-  });
+  it('submits the correct data upon submitting form', () => {
+    const { getByTestId } = render(<ContactForm />);
 
-  // Last name
-  it('displays an error message if last name is blank', () => {
+    const testObject = {
+      "firstName": "David",
+      "lastName": "White",
+      "email": "david@davidwhite.com",
+      "message": "This is a test"
+    };
     
-    act(async () => {
-      form.find('form').simulate('submit');
-      await wait(() => {
-        expect(getAllByTestId(/yup-error-lname/i)).not.toBeNull();
-      })
-    })
-  });
+    // Populate the form
+    // First name
+    const inputFname = getByTestId('firstname');
+    expect(inputFname.value).toBe("");
+    fireEvent.change(inputFname, { target: { value: testObject.firstName} });
+    expect(inputFname.value).toBe("David");
+    console.log(inputFname.value)
 
-  // Email
-  it('displays an error message if the email is blank', () => {
-    
-    act(async () => {
-      form.find('form').simulate('submit');
-      await wait(() => {
-        expect(getAllByTestId(/yup-error-email/i)).not.toBeNull();
-      })
-    })
-  });
+    // Last name
+    const inputLname = getByTestId('lastname');
+    expect(inputLname.value).toBe("");
+    fireEvent.change(inputLname, { target: { value: testObject.lastName} });
+    expect(inputLname.value).toBe("White");
+    console.log(inputLname.value)
+
+    // Email
+    const inputEmail = getByTestId('email');
+    expect(inputEmail.value).toBe("");
+    fireEvent.change(inputEmail, { target: { value: testObject.email} });
+    expect(inputEmail.value).toBe("david@davidwhite.com");
+    console.log(inputEmail.value)
+
+    // Message
+    const message = getByTestId('message');
+    expect(message.value).toBe("");
+    fireEvent.change(message, { target: { value: testObject.message} });
+    expect(message.value).toBe("This is a test");
+    console.log(message.value)
+
+
+    // Submit the form
+    const mockOnSubmit = jest.fn(data => data);
+    const button = getByTestId('submit');
+    button.onclick = function() {
+      const data = {
+        firstName: inputFname.value,
+        lastName: inputLname.value,
+        email: inputEmail.value,
+        message: message.value,
+      };
+      mockOnSubmit(data);
+    };
+    fireEvent.click(button);
+
+    // Compare data to the expected data
+    expect(mockOnSubmit.mock.results[0].value).toEqual(testObject);
+    expect(mockOnSubmit.mock.results[0].value).not.toEqual({});
+  })
 })
-    
-    
-    // Test for expected data type, regex match, and max-length (as applicable)
-
-      // Test data type of fields matches expected data type
-      // Create JSDoc documentation comments
-
-        /* First name
-          @param {string} firstName
-        */
-       // Expected: string
-       // Match: N/A
-       // Max-length: N/A
-
-        /* Last name
-          @param {string} lastName
-        */
-       // Expected: string
-       // Match: N/A
-       // Max-length: N/A
-
-        /* Email
-          @param {string} email
-        */
-       // Expected: string
-       // Match: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i
-       // Max-length: N/A
-
-        /* Message
-          @param {string} message
-        */
-       // Expected: string
-       // Match: N/A
-       // Max-length: N/A
